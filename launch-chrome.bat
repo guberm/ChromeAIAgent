30161a961ac97c93652111d131f59bd3f18760c0
Michael Guber <michael@guber.dev>
2025-09-15 20:30:33 -0400

    chore: clean repository by removing unnecessary files and adding .gitignore
    - Remove old version zip files (v2.2.0-v2.2.3) from Git tracking
    - Remove build artifacts (build.zip, launch-chrome.bat)
    - Remove backup files (original_sidepanel.html/js)
    - Add comprehensive .gitignore to prevent future tracking of unnecessary files

--- launch-chrome.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-"C:\Program Files\Google\Chrome\Application\chrome.exe" --load-extension="c:\ChromeAIAgent\build" --disable-web-security --user-data-dir="c:\ChromeAIAgent\temp-chrome-profile"
